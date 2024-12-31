d1aa5d4c52bc34e8f9bcaa0f4d25ba7a737454a9
Sumanjeet <sumanjeet0012@gmail.com>
2024-12-31 23:25:07 +0530

    some changes for CORS policy

--- user-management-app/backend/server.js
+++ user-management-app/backend/server.js
@@ -18,12 +18,8 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use(cors({
-//   origin: '*',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-// app.use(express.json());
+app.use(cors());  
+app.use(express.json());
 
 // Create MySQL connection
 const db = mysql.createConnection({
